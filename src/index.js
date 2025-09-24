77a89884e0ea6129c1660467574a175edc41e493
navjotmaan <navjotmaan137@gmail.com>
2025-09-24 14:55:14 +0000

    imported each js file and added the logic to show the content of each page on clicking the respective button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-import {content} from './home.js';
+import {loadHome} from './home.js';
+import {loadMenu} from './menu.js';
+import { loadContact } from './contact.js';
 
-document.addEventListener("DOMContentLoaded", content);
+loadHome();
+
+const content = document.querySelector('#content');
+
+const homeBtn = document.querySelector('#home');
+homeBtn.addEventListener("click", () => {
+    content.innerHTML = "";
+    loadHome();
+});
+
+
+const menuBtn = document.querySelector('#menu');
+menuBtn.addEventListener('click', () => {
+    content.innerHTML = "";
+    loadMenu();
+});
+
+const contactBtn = document.querySelector('#contact');
+contactBtn.addEventListener("click", () => {
+    content.innerHTML = "";
+    loadContact();
+});
