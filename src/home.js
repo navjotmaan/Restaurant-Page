4d6ab3b512540d23ab3c0572bf9fea8c0bf6ace4
navjotmaan <navjotmaan137@gmail.com>
2025-09-24 14:50:36 +0000

    added the function to load home page content

--- src/home.js
+++ src/home.js
@@ -1,18 +1,22 @@
 import cafe from './cafe.jpg';
 
-export const content = document.querySelector('#content');
+export function loadHome() {
+    const content = document.querySelector('#content');
 
-const restaurantName = document.createElement('h1');
-restaurantName.textContent = 'Maan Restaurant';
+    const restaurantName = document.createElement('h1');
+    restaurantName.textContent = 'Maan Restaurant';
 
-const image = document.createElement("img");
-image.src = cafe;
-image.style.width = '500px';
-image.style.height = '500px';
+    const image = document.createElement("img");
+    image.src = cafe;
+    image.style.width = '500px';
+    image.style.height = '500px';
 
-const message = document.createElement('p');
-message.textContent = 'We serve the best Pizza, Pasta, Noodles, and Coffee. Enjoy tasty food with aesthetic view of our restaurant. Book your place with your friends and family.'
+    const message = document.createElement('p');
+    message.textContent = 'We serve the best Pizza, Pasta, Noodles, and Coffee. Enjoy tasty food with aesthetic view of our restaurant. Book your place with your friends and family.'
 
-content.appendChild(restaurantName);
-content.appendChild(image);
-content.appendChild(message);
+    content.appendChild(restaurantName);
+    content.appendChild(image);
+    content.appendChild(message);
+
+    document.addEventListener("DOMContentLoaded", content);
+}
