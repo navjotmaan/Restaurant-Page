405d764de7de89f709421df0abc25ff4d38d62d9
navjotmaan <navjotmaan137@gmail.com>
2025-09-25 04:24:57 +0000

    added the image address

--- src/home.js
+++ src/home.js
@@ -17,10 +17,15 @@ export function loadHome() {
     message.setAttribute('id', 'message');
     message.textContent = 'Savor the art of dining with our innovative menu, crafted from the freshest ingredients. Perfect for food lovers seeking a modern twist on classic favorites. Experience great taste, impeccable service, and a vibe that keeps you coming back.'
 
+    const credit = document.createElement('span');
+    credit.classList.add('credits');
+    credit.innerHTML = 'Photo by <a href="https://unsplash.com/@moisamihai092?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Mihai Moisa</a> on <a href="https://unsplash.com/photos/black-chairs-Djtc1T38-GY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>'
+    
     container.appendChild(image);
     container.appendChild(message);
     content.appendChild(restaurantName);
     content.appendChild(container);
+    content.appendChild(credit);
 
     document.addEventListener("DOMContentLoaded", content);
 }
